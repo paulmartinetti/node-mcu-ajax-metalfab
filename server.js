6ca46158c897502ed5dbdd2e83092118dfdb73d4
Paul Martinetti <paulmartinetti@Pauls-MacBook-Air.local>
2021-02-20 09:15:25 -0500

    more console detail

--- server.js
+++ server.js
@@ -13,32 +13,37 @@ const { Script } = require('vm');
 // including handling JSON data
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 // this is where we'll handle our various routes from
 const routes = require('./routes/routes.js')(app, fs);
 
-var net = require('net');
-
-var serverN = net.createServer(function(socket) {
-	fs.readFile("./data/projet.json", 'utf-8', (err, data) => {
-		if (err) {
-			throw err;
-		}
-		socket.write(JSON.stringify(data, null, 2));
-		socket.pipe(socket);
-	});
-});
-
-serverN.listen(1337, '10.0.0.251');
-
 Script.js;
-
 
 //finally, launch our server on port 3001.
 const server = app.listen(3001, () => {
     console.log('listening on port %s...', server.address().port);
 });
 
-
-
+// socket for esp32 to fetch projet data
+var net = require('net');
+var serverN = net.createServer(function(socket) {
+	fs.readFile("./data/projet.json", 'utf-8', (err, data) => {
+		if (err) {
+			throw err;
+		}
+		socket.write(JSON.stringify(data));
+		socket.pipe(socket);
+	});
+	socket.on('end', () => {
+		console.log('client disconnected');
+	  });
+});
+serverN.on('error', (err) => {
+	throw err;
+  });
+//serverN.listen(1337, '10.0.0.250');
+//console.log('socket listening on port...',1337);
+serverN.listen(1337, () => {
+	console.log('server bound at port 1337');
+  });
